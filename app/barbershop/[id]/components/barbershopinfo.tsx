ded1e60270523972e4bd417a9034e5c5ab77710d
Felipe-Bignoto <felipebignoto@gmail.com>
2024-02-01 13:40:17 -0300

    feat:add service component

---
+++ app/barbershop/[id]/components/barbershopinfo.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import { Button } from '@/app/_components/ui/button'
+import { Barbershop } from '@prisma/client'
+import { ChevronLeftIcon, MenuIcon, MapPin, StarIcon } from 'lucide-react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+interface BarberShopInfoProps {
+  barbershop: Barbershop
+}
+
+const BarberShopInfo = ({ barbershop }: BarberShopInfoProps) => {
+  const router = useRouter()
+  const handleBackClick = () => {
+    router.push('/')
+  }
+  return (
+    <div>
+      <div className="h-[250px] w-full relative">
+        <Button
+          variant="outline"
+          size="icon"
+          className="z-50 absolute top-4 left-4"
+          onClick={handleBackClick}
+        >
+          <ChevronLeftIcon />
+        </Button>
+
+        <Button
+          variant="outline"
+          size="icon"
+          className="z-50 absolute top-4 right-4"
+        >
+          <MenuIcon />
+        </Button>
+
+        <Image
+          alt={barbershop.name}
+          src={barbershop.imageUrl}
+          fill
+          className="object-cover opacity-75"
+        ></Image>
+      </div>
+
+      <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
+        <h1 className="text-xl font-bold ">{barbershop.name}</h1>
+        <div className="flex items-center gap-1 mt-2">
+          <MapPin className="text-primary" size={18} />
+          <p className="text-sm">{barbershop.address}</p>
+        </div>
+        <div className="flex items-center gap-1 mt-2">
+          <StarIcon className="text-primary" size={18} />
+          <p className="text-sm">5,0 (899 avaliações)</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BarberShopInfo
---
+++ app/barbershop/[id]/page.tsx
@@ -0,0 +1,45 @@
+import { db } from '@/app/_lib/prisma'
+import BarberShopInfo from './components/barbershopinfo'
+import ServiceItem from './components/service-item'
+import { Service } from '@prisma/client'
+
+interface BarbershopDetailsPageProps {
+  params: {
+    id?: string
+  }
+}
+
+const BarbershopDetailsPage = async ({
+  params,
+}: BarbershopDetailsPageProps) => {
+  if (!params.id) {
+    // TODO: Redirecionar para home
+    return null
+  }
+
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  })
+  if (!barbershop) {
+    // TODO: Redirecionar para home
+    return null
+  }
+  return (
+    <div>
+      <BarberShopInfo barbershop={barbershop} />
+      <div className="px-5 flex flex-col gap-4 py-6 ">
+        {' '}
+        {barbershop.services.map((service: Service) => (
+          <ServiceItem service={service} key={service.id} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default BarbershopDetailsPage
